1afa2fef414c4c3db9afa9e7d8ec1c92b991ce05
KnowYourLines <knowyourlines@outlook.com>
2021-01-31 22:40:59 +0000

    made model

--- habitat/models.py
+++ habitat/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Result(models.Model):
+    unique_bid_number = models.CharField(max_length=255)
+    accepted_or_rejected = models.CharField(max_length=255)
+
+
+#             "Market Name":"DC LF",
+#             "Delivery Date":"2021-02-01",
+#             "Unique bid number":"20210201 AG-HEL00G",
+#             "Response Unit":"AG-HEL00G",
+#             "Unit type":"BM",
+#             "Agent/Applicant":"Habitat Energy Limited",
+#             "Related Entity":"N/A",
+#             "Volume offered":49,
+#             "Volume Accepted":49,
+#             "Delivery Start":"2021-01-31T23:00:00",
+#             "Delivery End":"2021-02-01T23:00:00",
+#             "Service Duration":24,
+#             "Availability Fee":16.99,
+#             "Total Cost":19980.24,
+#             "RTM/no RTM":"RTM",
+#             "Accepted/Rejected":"Accepted",
+#             "Rejection code":null,
+#             "Technology Type":"Battery"
